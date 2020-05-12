0fd67f2d3d0912077126ffc86ecc84db1ca20c68
shudingcai <shudingcai@163.com>
2020-05-12 13:53:19 +0800

    Initial commit

---
+++ app/src/main/java/kostal/com/myandroidutilslib/MainActivity.java
@@ -0,0 +1,14 @@
+package kostal.com.myandroidutilslib;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
